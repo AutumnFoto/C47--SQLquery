ea7b5bb7334cb6af310d9e30a96b4810cb22b6ac
Autumn Fotopoulos <acarte94@gmail.com>
2021-06-02 15:52:09 -0500

    part two

--- MusicHistory.sql
+++ MusicHistory.sql
@@ -1,2 +1,5 @@
-﻿/* 1. Query all of the entries in the Genre table
-SELECT * FROM Genre;
+﻿-- 1. Query all of the entries in the Genre table
+--SELECT * FROM Genre;
+--------------------------------------------------------
+--2. Query all the entries in the Artist table and order by the artist's name. HINT: use the ORDER BY keywords
+Select * from Artist order by ArtistName;
