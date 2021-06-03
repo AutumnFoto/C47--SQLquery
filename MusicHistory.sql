a764dc923b74ca2283d0441de17bd05d7ec71c76
Autumn Fotopoulos <acarte94@gmail.com>
2021-06-03 11:16:27 -0500

    phase 3

--- MusicHistory.sql
+++ MusicHistory.sql
@@ -2,4 +2,11 @@
 --SELECT * FROM Genre;
 --------------------------------------------------------
 --2. Query all the entries in the Artist table and order by the artist's name. HINT: use the ORDER BY keywords
-Select * from Artist order by ArtistName;
+--Select * from Artist order by ArtistName;
+
+----------------------------------------------------------
+--3. Write a SELECT query that lists all the songs in the Song table and include the Artist name
+SELECT s.*,
+       a.ArtistName
+  FROM Song s
+       LEFT JOIN Artist a on s.ArtistId = a.id;
